3a30f6060ae8b04315d676746029b2fa38621116
Enzo Caviglia <88462886+EnzoCavig@users.noreply.github.com>
2021-11-03 20:27:42 -0300

    parte 2 calculadora

---
+++ provacalculadora/calculadora.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+
+$um     = $_POST['1'];
+$dois   = $_POST['2'];
+$tres   = $_POST['3'];
+$quatro = $_POST['4'];
+$cinco  = $_POST['5'];
+$seis   = $_POST['6'];
+$sete   = $_POST['7'];
+$oito   = $_POST['8'];
+$nove   = $_POST['9'];
+$zero   = $_POST['0'];
+
+$adicao        = $_POST['adicao'];
+$subtracao     = $_POST['subtracao'];
+$multiplicacao = $_POST['multiplicacao'];
+$divisao       = $_POST['divisao'];
+
+$resultado = $_POST['resultado'];
+$reset = $_POST['reset'];
+$igual = $_POST['igual'];
+?>
